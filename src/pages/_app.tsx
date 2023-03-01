31b6cf6b46e87e8122bd988db82d2432a7d5de3b
VI Nguyen <nguyenhungvicb@gmail.com>
2023-03-01 17:20:28 +0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ import { API_URL } from "@/utils/config";
 import ReactGA from "react-ga";
 import { useRouter } from "next/router";
 
+ReactGA.initialize("UA-257495971-2");
 export const GlobalContext = createContext<any>(null);
 export default function App({ Component, pageProps }: AppProps) {
   const [loginToken, setLoginToken] = useState<string | null>(null);
@@ -56,9 +57,6 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [verifyUser]);
 
   useEffect(() => {
-    ReactGA.initialize("UA-257495971-2", {
-      standardImplementation: true,
-    });
     console.log({ asPath });
     if (typeof window !== "undefined") {
       ReactGA.pageview(window.location.pathname + window.location.search);
